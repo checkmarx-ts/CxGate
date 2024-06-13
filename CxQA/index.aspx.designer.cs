5cbc89b46074dde7bfbe9b4f661d8fc039db5f8e
CxElks <104453163+CxElks@users.noreply.github.com>
2024-06-13 20:31:26 +0000

    1. Font color changed 2. Added error messaging for failed logins 3.Darkened border added for scan comparison

--- CxQA/index.aspx.designer.cs
+++ CxQA/index.aspx.designer.cs
@@ -114,6 +114,15 @@ namespace CxQA
         protected global::System.Web.UI.HtmlControls.HtmlGenericControl divLoginForm;
 
         /// <summary>
+        /// lblErrorMessages control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblErrorMessages;
+
+        /// <summary>
         /// authDomainsDropDown control.
         /// </summary>
         /// <remarks>
